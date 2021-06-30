1bf0936a26f2a04cd692f8318e9f507b38185548
Yu-chan1023 <minami.15801580@gmail.com>
2021-06-30 10:13:33 +0900

    リスト追加及びプロジェクト追加機能作成

---
+++ src/components/AddList.vue
@@ -0,0 +1,60 @@
+<template>
+  <div style="min-width: 320px;">
+    <div style="background-color: #385F73; padding: 10px; border-radius: 6px; color: #FFF;">
+      <div
+        v-if="!showListInput"
+        @click="showListInput = true"
+      >+カテゴリー追加
+      </div>
+      <div v-else>
+        <input type="text" v-model="title" placeholder="カテゴリー追加" style="background-color: #FFF; width: 100%; padding: 5px; margin-bottom: 10px;">
+        <button @click="addList" class="add-list" style="margin-right: 5px;">追加</button>
+        <button @click="showListInput = false" class="cancel-list">キャンセル</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+export default {
+  props: {
+    filter: {
+      type: Number
+    }
+  },
+  data() {
+    return {
+      title: '',
+      showListInput: false
+    }
+  },
+  methods: {
+    addList() {
+      this.$store.dispatch('addList', { title: this.title, filter: this.filter })
+      this.title = ""
+      this.showListInput = false
+    }
+  }
+};
+</script>
+
+<style scoped>
+.add-list {
+  background-color: transparent;
+  border: 2px solid #FFF;
+  border-radius: 8px;
+  padding: 4px 8px;
+}
+.add-list:hover {
+  opacity: .7;
+}
+.cancel-list {
+  background-color: transparent;
+  border: 2px solid #FFF;
+  border-radius: 8px;
+  padding: 4px 8px;
+}
+.cancel-list:hover {
+  opacity: .7;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,9 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const taskProjectsData = localStorage.getItem("task-projects")
-const store =  new Vuex.Store({
+const labelLists = localStorage.getItem("label-lists")
+
+const store = new Vuex.Store({
   state: {
     projects: taskProjectsData ? JSON.parse(taskProjectsData) : [
       {
@@ -12,7 +14,7 @@ const store =  new Vuex.Store({
         lists: [
           {
             id: 1,
-            title: "テスト1",
+            title: "テスト4",
             cards: [
               { id: 1, title: "テスト1", description: "テスト1", priority: "高", inCharge:  "南"},
               { id: 2, title: "テスト2", description: "テスト2", priority: "中", inCharge:  "山本"},
@@ -39,34 +41,9 @@ const store =  new Vuex.Store({
               { id: 11, title: "テスト11", description: "テスト11", priority: "高", inCharge:  "松浦"},
               { id: 12, title: "テスト12", description: "テスト12", priority: "小", inCharge:  "酒井"},
             ]
-          }
-        ]
-      },
-      {
-        id: 2,
-        lists: [
-          { 
-            id: 1,
-            title: "テスト1",
-            cards: [
-              { id: 1, title: "テスト1", description: "テスト1", priority: "高", inCharge:  "南"},
-              { id: 2, title: "テスト2", description: "テスト2", priority: "中", inCharge:  "山本"},
-              { id: 3, title: "テスト3", description: "テスト3", priority: "高", inCharge:  "松浦"},
-              { id: 4, title: "テスト4", description: "テスト4", priority: "小", inCharge:  "酒井"},
-            ]
           },
           { 
-            id: 2,
-            title: "テスト2",
-            cards: [
-              { id: 5, title: "テスト5", description: "テスト5", priority: "高", inCharge:  "南"},
-              { id: 6, title: "テスト6", description: "テスト6", priority: "中", inCharge:  "山本"},
-              { id: 7, title: "テスト7", description: "テスト7", priority: "高", inCharge:  "松浦"},
-              { id: 8, title: "テスト8", description: "テスト8", priority: "小", inCharge:  "酒井"},
-            ]
-          },
-          { 
-            id: 3,
+            id: 4,
             title: "テスト3",
             cards: [
               { id: 9, title: "テスト9", description: "テスト9", priority: "高", inCharge:  "南"},
@@ -78,22 +55,56 @@ const store =  new Vuex.Store({
         ]
       }
     ],
-    labels: [
+    labels: labelLists ? JSON.parse(labelLists) : [
       {
         id: 1,
-        name: "オウンドメディア" 
-      },
-      {
-        id: 2,
-        name: "コーポレートサイト" 
+        name: 'オウンドメディア'
       }
-    ]
+    ],
+    filter: 1
   },
   mutations: {
+    addList(state, payload) {
+      state.projects[payload.filter - 1].lists.push({
+        id: state.projects[payload.filter - 1].length + 1,
+        title: payload.title,
+        cards: []
+      })
+    },
+    removeList(state, payload) {
+      state.projects[payload.filter - 1].lists.splice(payload.listIndex, 1)
+    },
+    addProject(state, payload) {
+      state.projects.push({
+        id: state.projects.length + 1,
+        lists: payload.lists
+      })
+      state.labels.push({
+        id: state.labels.length + 1,
+        name: payload.name
+      })
+    },
+    changeFilter (state, {filter}) {
+      state.filter = filter
+    },
   },
   actions: {
+    addList({commit}, payload) {
+      commit('addList', payload)
+    },
+    removeList({commit}, payload) {
+      commit('removeList', payload)
+    },
+    addProject({commit}, payload) {
+      commit('addProject', payload)
+    }
   },
   getters: {
+    filteredLabel(state) {
+      return state.projects.filter(function(project) {
+        return project.id === state.filter
+      })
+    }
   }
 })
 
@@ -101,4 +112,5 @@ export default store
 
 store.subscribe((mutation, state) => {
   localStorage.setItem('task-projects', JSON.stringify(state.projects))
+  localStorage.setItem('label-lists', JSON.stringify(state.labels))
 })
